1a229af760f07d53c96b5f5d8ba5056553537139
Jakub Biesiada <biesiadajakub@icloud.com>
2019-12-28 00:38:15 +0100

    Update config

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,26 @@
-import resolve from 'rollup-plugin-node-resolve'
-import typescript from 'rollup-plugin-typescript2'
-import { terser } from 'rollup-plugin-terser'
-import commonjs from 'rollup-plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
 
-import pkg from './package.json'
+import pkg from './package.json';
 
 export default {
-  input: ['src/index.ts'],
+  input: 'src/index.ts',
   output: [
     {
       file: pkg.main,
       format: 'es',
-      sourcemap: true,
-    },
+      sourcemap: true
+    }
   ],
-  external: [
-    'react',
-    'prop-types'
-  ],
+  external: ['react'],
   plugins: [
     resolve(),
     typescript(),
     terser(),
     commonjs({
-      include: 'node_modules/**',
-    }),
-  ],
-}
+      include: 'node_modules/**'
+    })
+  ]
+};
