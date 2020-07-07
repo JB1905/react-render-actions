22b2205852b2a61325d41eda61bb2d359ed20d1b
Jakub Biesiada <biesiadajakub@icloud.com>
2020-07-07 19:42:45 +0200

    feat: add loops

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+export { For } from './Loops/For';
+export { ForOf } from './Loops/ForOf';
+export { ForIn } from './Loops/ForIn';
+export { ForEach } from './Loops/ForEach';
+export { While } from './Loops/While';
+export { DoWhile } from './Loops/DoWhile';
+export { Break } from './Loops/Break';
+export { Continue } from './Loops/Continue';
+
 export { SetTimeout } from './Timeouts/SetTimeout';
 export { SetInterval } from './Timeouts/SetInterval';
 
