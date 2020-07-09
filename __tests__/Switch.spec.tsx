80366c69fa2e2baf32f09831c8144978fc5c55b0
Jakub Biesiada <biesiadajakub@icloud.com>
2020-07-09 13:59:40 +0200

    test: update tests

--- __tests__/Switch.spec.tsx
+++ __tests__/Switch.spec.tsx
@@ -4,10 +4,11 @@ import { render } from '@testing-library/react';
 import { Switch, Case, Default } from '../src';
 
 describe('Switch', () => {
-  it('should ', () => {
-    const switchValue = 'banana';
-
-    const { container } = render(
+  it.each([
+    ['banana', 'Banana'],
+    ['cherry', 'Fruit not found'],
+  ])('should return $result for $switchValue', (switchValue, result) => {
+    const { container, getByText } = render(
       <Switch switchValue={switchValue}>
         <Case case="apple">Apple</Case>
         <Case case="banana">Banana</Case>
@@ -18,6 +19,33 @@ describe('Switch', () => {
       </Switch>
     );
 
-    expect(container.firstChild).toBe(null);
+    expect(container).toMatchSnapshot();
+
+    expect(getByText(result)).toBeDefined();
+  });
+
+  it('should return neither case nor default', () => {
+    const { container } = render(
+      <Switch switchValue="pineapple">
+        <Case case="apple">Apple</Case>
+        <Case case="pear">Pear</Case>
+      </Switch>
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should return first case matching switchValue', () => {
+    const { container } = render(
+      <Switch switchValue="apple">
+        <Case case="apple">Apple 1</Case>
+        <Case case="pear">Pear</Case>
+        <Case case="apple">Apple 2</Case>
+        <Case case="apple">Apple 3</Case>
+        <Case case="banana">Banana</Case>
+      </Switch>
+    );
+
+    expect(container).toMatchSnapshot();
   });
 });
