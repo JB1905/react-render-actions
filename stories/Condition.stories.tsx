4abee8a1b227602449ec51faee26ac9da445c499
Jakub Biesiada <biesiadajakub@icloud.com>
2020-12-08 22:30:25 +0100

    chore: update stories

--- stories/Condition.stories.tsx
+++ stories/Condition.stories.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { boolean } from '@storybook/addon-knobs';
+import { Meta, Story } from '@storybook/react';
 
 import { If, Else, Then, When, Unless } from '../src';
 
+interface Props {
+  readonly condition: boolean;
+}
+
 export default {
   title: 'Condition',
-};
+  // subcomponents: { If, Else, Then, When, Unless },
+  args: {
+    condition: true,
+  },
+} as Meta;
 
-export const ifElse = () => (
-  <If condition={boolean('condition', true)}>
+export const ifElse: Story<Props> = (args) => (
+  <If {...args}>
     <Then>
       <p>True</p>
     </Then>
@@ -19,14 +27,18 @@ export const ifElse = () => (
   </If>
 );
 
-export const when = () => (
-  <When condition={boolean('condition', true)}>
+export const when: Story<Props> = (args) => (
+  <When {...args}>
     <p>True</p>
   </When>
 );
 
-export const unless = () => (
-  <Unless condition={boolean('condition', false)}>
+export const unless: Story<Props> = (args) => (
+  <Unless {...args}>
     <p>False</p>
   </Unless>
 );
+
+unless.args = {
+  condition: false,
+};
--- stories/Switch.stories.tsx
+++ stories/Switch.stories.tsx
@@ -1,20 +1,36 @@
 import React from 'react';
+import { Meta, Story } from '@storybook/react';
 
 import { Switch, Case, Default } from '../src';
 
 export default {
   title: 'Switch',
-};
+  // component: Switch,
+  // subcomponents: { Case, Default },
+  args: {
+    value: 'apple',
+    cases: [
+      {
+        case: 'banana',
+        text: 'Banana',
+      },
+      {
+        case: 'apple',
+        text: 'Apple',
+      },
+    ],
+  },
+} as Meta;
 
-export const normal = () => (
-  <Switch switchValue="apple">
-    <Case case="banana">
-      <p>Banana</p>
-    </Case>
+export const normal: Story = ({ value, cases }) => (
+  <Switch switchValue={value}>
+    {/* {cases.map(case => (
+      <Case case={case}>
+        <p>{text}</p>
+      </Case>
+      ))} */}
 
-    <Case case="apple">
-      <p>Apple</p>
-    </Case>
+    {/* {cases.map((case) => <p>{text}</p>)} */}
 
     <Default>
       <p>Fruit not found :(</p>
